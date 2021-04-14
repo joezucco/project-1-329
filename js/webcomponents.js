ff5365e970413f2af6408d7448ee071e8d24654f
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-04-13 20:29:25 -0400

    start work on web component

--- js/webcomponents.js
+++ js/webcomponents.js
@@ -4,18 +4,23 @@
 // Web Components Classes Here
 //////////////////////////////////
 
-// class TemplateExample extends HTMLElement {
-//     constructor(){
-//         super()
+class MyCard extends HTMLElement {
+    constructor(){
+        super()
 
-//         this.innerHTML = `<h1>Hello World</h1>`
-//     }
-// }
+        const name = this.getAttribute("name")
+        const git = this.getAttribute("git");
+        const live = this.getAttribute("live");
+        const image = this.getAttribute("image");
+
+        console.log(name, git, live, image)
+    }
+}
 
 
 
-/////////////////////////////////
-// Register the HTML Tag here
-/////////////////////////////////
+///////////////////////////////
+// Register the HTML Tag
+///////////////////////////////
 
-// customElements.define("template-example", TemplateExample)
+customElements.define("my-card", MyCard)
