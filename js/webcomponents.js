60b97c34709c7a5fa7f8bbe31ae59f69a1fd9aca
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-04-13 20:52:38 -0400

    card componenet implemented

--- js/webcomponents.js
+++ js/webcomponents.js
@@ -5,22 +5,29 @@
 //////////////////////////////////
 
 class MyCard extends HTMLElement {
-    constructor(){
-        super()
+  constructor() {
+    super();
 
-        const name = this.getAttribute("name")
-        const git = this.getAttribute("git");
-        const live = this.getAttribute("live");
-        const image = this.getAttribute("image");
+    const name = this.getAttribute("name");
+    const git = this.getAttribute("git");
+    const live = this.getAttribute("live");
+    const img = this.getAttribute("image");
 
-        console.log(name, git, live, image)
-    }
+    this.innerHTML = `
+        <sl-card>
+        <h1>${name}</h1>
+        <img src=${img} alt=${name} slot="image">
+        <sl-button-group>
+        <a href=${git}><sl-button>Left</sl-button></a>
+        <a href=${live}><sl-button>Right</sl-button></a>
+        </sl-button-group>
+        </slcard>
+        `;
+  }
 }
-
-
 
 ///////////////////////////////
 // Register the HTML Tag
 ///////////////////////////////
 
-customElements.define("my-card", MyCard)
+customElements.define("my-card", MyCard);
